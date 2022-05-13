c5da13b5ebcc6143f9693b4f6ae2c36d2118a6d8
Ilgiz Temi <temirbekov91@gmail.com>
2022-05-13 14:52:30 -0400

    reviews dom project

---
+++ app.js
@@ -0,0 +1,114 @@
+const reviews = [
+    {
+      id: 1,
+      fullName: "Susan Smith",
+      specialty: "Frontend developer",
+      img:
+        "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
+      text:
+        "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry"
+    },
+    {
+      id: 2,
+      fullName: "Anna Johnson",
+      specialty: "web designer",
+      img:
+        "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg",
+      text:
+        "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal."
+    },
+    {
+      id: 3,
+      fullName: "Peter Jones",
+      specialty: "intern",
+      img:
+        "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
+      text:
+        "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag."
+    },
+    {
+      id: 4,
+      fullName: "Bill Anderson",
+      specialty: "the boss",
+      img:
+        "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
+      text:
+        "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic."
+    },
+    {
+        id: 5,
+        fullName: "Richard John",
+        specialty: "Product Manager",
+        img:
+          "https://images.pexels.com/photos/3748221/pexels-photo-3748221.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        text:
+          "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "
+      },
+      {
+        id: 6,
+        fullName: "Darren Milovich",
+        specialty: "Project Manager",
+        img:
+          "https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        text:
+          " Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown. Ier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb"
+      },
+      {
+        id: 7,
+        fullName: "Mila Gucci",
+        specialty: "DevOps",
+        img:
+          "https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        text:
+          "Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry.  sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris "
+      },
+  ];
+
+const img = document.querySelector('#img');
+const fullName = document.querySelector('#fullName')
+const specialty = document.querySelector('#specialty');
+const text = document.querySelector('#text');
+
+const prev = document.querySelector('.fa-angle-left');
+const next = document.querySelector('.fa-angle-right');
+const btn = document.querySelector('#btn');
+
+let profile = 0;
+
+window.addEventListener('DOMContentLoaded', () => {
+    const page = reviews[profile];
+    img.src = page.img;
+    fullName.innerHTML = page.fullName;
+    specialty.innerHTML = page.specialty;
+    text.innerHTML = page.text;
+});
+
+function newPage(props) {
+    const page = reviews[props];
+    img.src = page.img;
+    fullName.innerHTML = page.fullName;
+    specialty.innerHTML = page.specialty;
+    text.innerHTML = page.text;
+}
+
+prev.addEventListener('click', () => {
+    profile--;
+    if(profile < 0) {
+        profile = reviews.length - 1;
+    }
+    newPage(profile);
+});
+next.addEventListener('click', () => {
+    profile++;
+    if(profile > reviews.length - 1) {
+        profile = 0;
+    }
+    newPage(profile);
+});
+
+btn.addEventListener('click', () => {
+    const min = 0;
+    const max = reviews.length - 1;
+    profile = Math.floor(Math.random() * (max - min + 1)) + min;
+    newPage(profile);
+})
